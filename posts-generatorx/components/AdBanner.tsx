37d83cc0cd1b1bc829a89e9946d51dd9b89b2a7c
Sidy60 <146986079+Sidy60@users.noreply.github.com>
2026-04-03 02:33:48 +0000

    Update AdBanner.tsx

--- posts-generatorx/components/AdBanner.tsx
+++ posts-generatorx/components/AdBanner.tsx
@@ -1,12 +1,15 @@
-import { AdMobBanner } from 'expo-ads-admob';
 import { View } from 'react-native';
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 export default function AdBanner() {
   return (
     <View style={{ marginTop: 20 }}>
-      <AdMobBanner
-        bannerSize="smartBannerPortrait"
-        adUnitID="ca-app-pub-5350081816144613/1395585176"
+      <BannerAd
+        unitId="ca-app-pub-5350081816144613/1395585176"
+        size={BannerAdSize.BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
       />
     </View>
   );
